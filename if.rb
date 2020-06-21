419b44a4d1f7c74d7800a2cec697d02c73395a4c
liupeng <liupeng_internet@163.com>
2020-06-21 10:40:22 +0800

    first commit

---
+++ if.rb
@@ -0,0 +1,13 @@
+input=gets.to_i
+# if input == 2
+#   puts "equal is right"
+#   system "妈妈问我怎么不开心"
+# elsif input == 100
+#   puts "一百块都不给我"
+
+# unless 使用
+unless input == 100
+  puts "不等于100"
+    else
+      puts "等于100"
+end
